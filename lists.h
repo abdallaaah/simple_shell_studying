9c4fdd94529df719dec4441a95954840aa2c3ad4
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-24 18:15:16 -0700

    tst

---
+++ lists.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+char **split_to_array(char* line);
+char **commands_to(char* str);
+char** spliting(char* line);
+void execute_me(char** argv);
+void print_env();
+#endif
--- split_to_array.c
+++ split_to_array.c
@@ -1,4 +1,4 @@
-#include "lists_mod.h"
+#include "lists.h"
 /**
  * split_to_array - it used for spliting the line from main to tokenz in array
  * @line: the line by user from the standard input
@@ -14,23 +14,23 @@ int count = 1;
 line_2 = malloc(strlen(line));
 if (line_2 == NULL)
 {
-perror("(Error in malloc and memory size)\n");
+perror("(Error in malloc and memory size)");
 return (NULL);
 }
 line_2 = strcpy(line_2, line);
-token = strtok(line, " \t\r\n\a");
+token = strtok(line, " ");
 while (token != NULL)
 {
-token = strtok(NULL, " \t\r\n\a");
 count++;
-}
+token = strtok(NULL, " ");
+};
 sttr = malloc(sizeof(char *) * count);
-token_2 = strtok(line_2, " \t\r\n\a");
+token_2 = strtok(line_2, " ");
 for (i = 0; token_2 != NULL; i++)
 {
 sttr[i] = malloc(sizeof(char) * (strlen(token_2)));
 strcpy(sttr[i], token_2);
-token_2 = strtok(NULL, " \t\r\n\a");
+token_2 = strtok(NULL, " ");
 }
 sttr[i] = NULL;
 free(line_2);
