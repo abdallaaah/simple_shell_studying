3cae10d796dce43c6bf0c1987342bcd2c5131487
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-05-25 16:48:07 -0700

    check after betty solving

--- task_no_arguments.c
+++ task_no_arguments.c
@@ -1,57 +1,47 @@
 #include "lists.h"
 /**
- * main - the main function that loop while exectution the command of the stdinput
- * Return: no return it is void
+ * main - that loop while exectution the command of the stdinput
+ * @argc: the argc is the number of the arguments
+ * @argv: arguments it self
+ * Return: it is void no return
  */
 int main(int argc, char *argv[])
 {
 char *args[2];
-char *line = NULL;
+char *line = NULL, *dot = NULL;
 char *dollar = "#cisfun$ ";
-int status;
+int status, handle = 0;
 size_t len;
 ssize_t nread;
 pid_t pid;
-int handle = 0;
-char *dot = NULL;
 dot = argv[0];
 while (1 && handle == 0 && argc)
 {
 if (isatty(fileno(stdin)))
-{
 write(STDOUT_FILENO, dollar, strlen(dollar));
+else
+handle = 1;
+nread = getline(&line, &len, stdin);
+if (line[nread - 1] == '\n')
+line[nread - 1] = '\0';
+if (nread == -1 && handle == 0)
+perror("Error in the getline function"), free(line), exit(EXIT_FAILURE);
+args[0] = line, args[1] = NULL, pid = fork();
+if (pid == -1)
+{
+perror("fork error"), exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+if (execve(args[0], args, environ) == -1)
+{
+perror(dot), exit(EXIT_FAILURE);
+}
 }
 else
 {
-handle = 1;
-}
-nread = getline(&line, &len, stdin);
-if (line[nread - 1] == '\n'){
-line[nread - 1] = '\0';
-}
-if (nread == -1 && handle == 0)
-{
-perror("Error in the getline function");
-free(line);
-exit(EXIT_FAILURE);
-}
-args[0] = line;
-args[1] = NULL;
-pid = fork();
-if (pid == -1)
-{
-perror("fork error");
-exit(EXIT_FAILURE);
-}
-else if (pid == 0){
-if (execve(args[0], args, environ) == -1){
-perror(dot);
-exit(EXIT_FAILURE);
-}
-}
-else{
 waitpid(pid, &status, 0);
 }
 }
-return 0;
+return (0);
 }
